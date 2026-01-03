f8ecdd1158b72a0fc8d5ab68981b50e8b0a67abf
Hocker AGI <contacto.hocker@gmail.com>
2026-01-02 18:22:08 -0800

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -8,15 +8,18 @@ const ASSETS = [
   "/assets/logo-score.webp",
   "/assets/hero.webp",
   "/assets/fondo-pagina-score.webp",
-  "/assets/icons/icon-192.png"
+  "/assets/icons/icon-192.png",
+  "/assets/icons/icon-512.png"
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keyList) => Promise.all(keyList.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); }))));
+});
+
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
